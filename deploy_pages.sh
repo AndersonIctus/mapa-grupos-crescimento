9a6b4b48944248058bef42fae62e5ada0d6cc147
Anderson Dourado <anderson.ictus@gmail.com>
2024-09-05 20:33:17 -0300

    Ajustando apra usar href para o nome do projeto - part 8

--- deploy_pages.sh
+++ deploy_pages.sh
@@ -1,6 +1,6 @@
 npm run pages
-# mv docs/browser/index.html docs
-mv docs/browser docs/mapa-grupos-crescimento
-# cp docs/index.html docs/404.html
+mv docs/browser/index.html docs
+mv docs/browser docs
+cp docs/index.html docs/404.html
 
 git add .
